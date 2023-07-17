3dbbc8a1bdbca750833693a08056856b00c3cef8
Josué Rodríguez-Ramos <josue.a.rodriguez.ramos@gmail.com>
2023-07-17 15:40:33 -0600

    Update 3_read_mapping_2500_genes.sh

--- gene_resolved/3_read_mapping_2500_genes.sh
+++ gene_resolved/3_read_mapping_2500_genes.sh
@@ -25,14 +25,13 @@ cd "$element"_mapping
 bowtie2 -D 10 -R 2 -N 1 -L 22 -i S,0,2.50 -p 20 -x /home/projects-wrighton/Trivedi_soil_fire/reads/completed_assemblies/gene_resolved_metaG_analyses/2500_size_filtered/gene_DB -S "$element".sam -1 /home/projects-wrighton/Trivedi_soil_fire/reads/processed_reads/"$element"_1_trimmed.fastq -2 /home/projects-wrighton/Trivedi_soil_fire/reads/processed_reads/"$element"_2_trimmed.fastq #run bowtie2 with indexed genomes. This is basically preset of --fast option specified in bowtie2 manual. http://bowtie-bio.sourceforge.net/bowtie2/manual.shtml
 echo "begin samtools"
 samtools view -@ 20 -bS "$element".sam > "$element".bam #sam to bam
-reformat.sh -Xmx100g idfilter=0.97 pairedonly=t primaryonly=t in="$element".bam out="$element"_mapped_97ID.FILTERED.bam #this will then give you the actual 97% ID mapping. This was wrong before on bbmap as the 97% ID cutoffs were not actually cutoffs.
-samtools sort -T "$element".97ID.sorted -o "$element".97ID.sorted.bam "$element"_mapped_97ID.FILTERED.bam -@ 20 #sort the bam file
-mv "$element".97ID.sorted.bam /home/projects-wrighton/Trivedi_soil_fire/reads/completed_assemblies/gene_resolved_metaG_analyses/2500_size_filtered/read_mapping_genes_output 
+reformat.sh -Xmx100g idfilter=0.95 pairedonly=t primaryonly=t in="$element".bam out="$element"_mapped_95ID.FILTERED.bam #this will then give you the actual 95% ID mapping.
+mv "$element".95ID.sorted.bam /home/projects-wrighton/Trivedi_soil_fire/reads/completed_assemblies/gene_resolved_metaG_analyses/2500_size_filtered/read_mapping_genes_output 
 
 echo "Finished mapping and samtools for '$element' in list" 
 #clean up the files
 #rm -r "$element".sam
 #rm -r "$element".bam
-#rm -r "$element"_mapped_97ID.FILTERED.bam
+#rm -r "$element"_mapped_95ID.FILTERED.bam
 cd /home/projects-wrighton/Trivedi_soil_fire/reads/completed_assemblies/gene_resolved_metaG_analyses/2500_size_filtered #move back up to root and restart the process.
 done
