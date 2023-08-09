82ef306f9da5eeb0234d888b1d6d2e2966d4d774
Josué Rodríguez-Ramos <josue.a.rodriguez.ramos@gmail.com>
2023-08-09 12:39:01 -0600

    Update 3_map_reads_for_coassembly_alt_mapBins.sh

--- read_processing_and_assembly/3_map_reads_for_coassembly_alt_mapBins.sh
+++ read_processing_and_assembly/3_map_reads_for_coassembly_alt_mapBins.sh
@@ -15,3 +15,6 @@ do
 bbmap.sh -Xmx48G threads=20 semiperfectmode=t overwrite=t ref=/home/projects-wrighton/Trivedi_soil_fire/reads/completed_assemblies/_bins/single_assembly_bins/all_255_non-drepped_bins_single_assemblies.fasta in1=/home/projects-wrighton/Trivedi_soil_fire/reads/processed_reads/"$element"_1_trimmed.fastq in2=/home/projects-wrighton/Trivedi_soil_fire/reads/processed_reads/"$element"_2_trimmed.fastq out="$element"_mapped.sam outu="$element"_unmapped_reads_combined.fastq
 reformat.sh in="$element"_unmapped_reads_combined.fastq out1="$element"_unmapped_reads_R1.fq out2="$element"_unmapped_reads_R2.fq
 done
+
+#How to find total number of reads mapped:
+samtools view -c -F 4 <bam>
